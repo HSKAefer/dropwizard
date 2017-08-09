fd160e6556362f5d2bdb6544a88cd7457f4d487f
Daniel Schaefer <daniel.schaefer03@sap.com>
2017-08-09 09:35:25 +0200

    20170809 added some angularjs frontend action

--- btp/src/main/java/de/dokukaefer/btp/BTPApplication.java
+++ btp/src/main/java/de/dokukaefer/btp/BTPApplication.java
@@ -4,6 +4,7 @@ import de.dokukaefer.btp.core.Team;
 import de.dokukaefer.btp.db.TeamDAO;
 import de.dokukaefer.btp.res.TeamResource;
 import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
 import io.dropwizard.db.DataSourceFactory;
 import io.dropwizard.hibernate.HibernateBundle;
 import io.dropwizard.migrations.MigrationsBundle;
@@ -24,13 +25,23 @@ public class BTPApplication extends Application<BTPConfiguration>{
 	            }
 	        };
 	
-	        @Override
-	        public String getName() {
-	            return "Badminton Tournament Platform";
-	        }
+    @Override
+    public String getName() {
+    	return "Badminton Tournament Platform";
+    }
 	  
 	@Override
 	public void initialize(Bootstrap<BTPConfiguration> bootstrap) {
+		// Map requests to /${1} to be found in the class path at /webContent/${1}
+		// see: https://github.com/dropwizard-bundles/dropwizard-configurable-assets-bundle
+		// configured assets bundle via bootstrap.addBundle(new ConfiguredAssetsBundle("/webContent/", "/", "btp.html"));
+		// info: assets bundle needs to be added in pom.xml
+		bootstrap.addBundle(new AssetsBundle("/webContent/", "/", "btp.html"));
+		// the MigrationsBundle is required to execute ..*.jar db migrate config.yml
+		// it is possible to refactor databases with xml/yaml/json/sql formats
+		// here we use the migrations.xml file in src/main/resources
+		// see: http://www.liquibase.org/
+		// info: requires the dropwizard-migrations artifact in pom.xml
 		bootstrap.addBundle(new MigrationsBundle<BTPConfiguration>() {
 			@Override
 			public DataSourceFactory getDataSourceFactory(BTPConfiguration configuration) {
@@ -44,6 +55,10 @@ public class BTPApplication extends Application<BTPConfiguration>{
 	public void run(BTPConfiguration configuration, Environment environment) throws Exception {
 		final TeamDAO teamDAO = new TeamDAO(hibernateBundle.getSessionFactory());
 		environment.jersey().register(new TeamResource(teamDAO));
+		// changes the application root path. since dropwizard 0.8.0 it can be changed in the yaml file via
+		// applicationContextPath: /
+		//rootPath: /application
+		//environment.jersey().setUrlPattern("/application/*");
 	}
 	
 	
--- btp/src/main/java/de/dokukaefer/btp/BTPConfiguration.java
+++ btp/src/main/java/de/dokukaefer/btp/BTPConfiguration.java
@@ -8,11 +8,13 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
 import io.dropwizard.db.DataSourceFactory;
 
-public class BTPConfiguration extends Configuration {
+public class BTPConfiguration extends Configuration {//implements AssetsBundleConfiguration {
 	
 	@Valid
     @NotNull
     private DataSourceFactory database = new DataSourceFactory();
+	
+	
 	
 	 @JsonProperty("database")
 	    public DataSourceFactory getDataSourceFactory() {
@@ -23,4 +25,14 @@ public class BTPConfiguration extends Configuration {
 	    public void setDataSourceFactory(DataSourceFactory dataSourceFactory) {
 	        this.database = dataSourceFactory;
 	    }
+
+// not required right now - maybe later	 
+//	@Override
+//	public AssetsConfiguration getAssetsConfiguration() {
+//		return assets;
+//	}
+//	
+//	@Valid
+//	@NotNull
+//	private AssetsConfiguration assets = AssetsConfiguration.builder().build();
 }
