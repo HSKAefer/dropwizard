7c76d067cf5c0d3dd847cba48c15b03fa3e2fd61
Daniel Schaefer <daniel.schaefer03@sap.com>
2017-11-06 09:49:58 +0100

    20171106 added healthcheckresource

--- btp/src/main/java/de/dokukaefer/btp/core/Game.java
+++ btp/src/main/java/de/dokukaefer/btp/core/Game.java
@@ -3,6 +3,7 @@ package de.dokukaefer.btp.core;
 import java.util.Date;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
